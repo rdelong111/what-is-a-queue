9643b39d4d38b8d1ff6df6dbc94b8215033be4d3
RyanDeLong <rdelong111@gmail.com>
2022-03-09 20:49:01 -0600

    commit tree 4d7a79f53f85cb736ef18a4d126ecc14dcf2a36b

--- javascript/queue.js
+++ javascript/queue.js
@@ -9,43 +9,63 @@ class Queue {
   // add item to rear of queue if not full
   // if full throw error
   enqueue(item) {
-
+    if (this.queue.length < this.limit) {
+      this.queue.push(item);
+    }
+    else {
+      throw Error('Error');
+    }
   }
 
   // remove item from front of queue and return it
   dequeue() {
-
+    return this.queue.shift();
   }
 
   // return item at front of queue without removing it
   peek() {
-
+    return this.queue[0];
   }
 
   // return true if queue is empty, otherwise false
   isEmpty() {
-
+    if (this.queue.length === 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   // return true if queue is full, otherwise false
   isFull() {
-
+    if (this.queue.length === this.limit) {
+      return true;
+    }
+    else {
+      return false;
+    }
   }
 
   // return number of items in queue
   size() {
-
+    return this.queue.length;
   }
 
   // return -1 if item not in queue, otherwise integer representing 
   // how far it is from the front
   search(target) {
-
+    for (let i = 0; i < this.queue.length; i++) {
+      if (this.queue[i] === target) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   // print contents of queue: do not return the queue itself!
   print() {
-    
+    console.log(this.queue);
   }
 }
 
